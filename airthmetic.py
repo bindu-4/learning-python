90bd34a52fa84f3b76c0cdd246830675f1eb202f
bindu-4 <bindughimire009@gmail.com>
2026-08-11 12:37:19 +0900

    Initial commit

---
+++ airthmetic.py
@@ -0,0 +1,11 @@
+#wap to input two numbers and find their sum,difference,productand quotient
+a = int (input("a="))
+b = int (input("b="))
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+print(sum)
+print(difference)
+print(product)
+print(quotient)
---
+++ areaperimeter.py
@@ -0,0 +1,7 @@
+#wap to input the length and breadth of a recyangle and calculate its area and perimeter
+length = float (input("Enter a length"))
+breadth = float(input ("Enetr a breadth"))
+area = length * breadth
+perimeter = 2 * (length * breadth)
+print(area)
+print(perimeter)
