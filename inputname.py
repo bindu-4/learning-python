7f7c48e1f4bfe0e833812afbac4f53084b5eb247
bindu-4 <bindughimire009@gmail.com>
2026-08-11 11:40:08 +0900

    Initial commit

---
+++ inputname.py
@@ -0,0 +1,7 @@
+#wap to input your name,age and adress and display them.
+Name = input("Enter a name")
+Age = int (input("Enter a age"))
+Address = input("Enetr a address")
+print(Name)
+print(Age)
+print(Address)
